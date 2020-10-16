31abf91efabc8511582b724681b524ba7dfd083d
ethanrochev <542272@my.cuhsd.org>
2020-10-16 18:58:56 +0000

    added the draw_axes function. draws the x- and y-axes 100 pixels long each going in each direction.

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ for i in range(6):
   trtl.forward(10)
   trtl.left(15)
 trtl.penup()
+
+''''
+draws the x- and y-axes 100 pixels long 
+each going in each direction.
+''''
+
+def draw_axes():
+  #centers at origin
+  trtl.goto(0,0)
+  trtl.pendown()
+  #draws the y- axis
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.forward(100)
+  trtl.left(90)
+  #draws the x-axis
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.pendown()
+
+draw_axes()
